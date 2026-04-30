b0928c1f7a33dfd25b83a257736d64187c7a3c5a
Pradeep M Doddakaragi <pradeepmallappadoddakaragi@gmail.com>
2026-04-30 19:21:03 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-    host: '0.0.0.0',
-  }
-})
+export default {
+  base: '/code-judge/',
+}
